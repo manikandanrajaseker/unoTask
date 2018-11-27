afc5225d5e654fd1be967e9ad6139ee0fddc1c15
manikandanrajasekar <rockmanmani123@gmail.com>
2018-11-27 12:50:52 +0530

    latest App

--- uno.js
+++ uno.js
@@ -6,11 +6,15 @@ var computerCardDraw="";
 var currentOpenCardNumber="";
 var currentOpenCardColor="";
 var changedColor="";
-var playerTurn=true;
-var computerTurn=false;
+var gameTurn="player";
+var wIndex="";
+var playerName="";
 function startGame(){
+    playerName=document.getElementById('playerName').value;
+    console.log(playerName);
     document.getElementById('entryForm').innerHTML="";
     document.getElementById('gameView').style.display='block';
+    
 }
 function drawCard(byWhom){
     
@@ -44,29 +48,49 @@ else if(byWhom=="openCard"){
     currentOpenCardColor=randColor;
 document.getElementById('droppedCard').innerHTML='<div class="card num-'+randamNumber+' '+randColor+'"><span class="inner"><span class="mark">'+changedRand+'</span></span></div>';
 }
-
+ if(byWhom=="computerCardView"){
+     if(computerCardCount>=7){
+         if(randamNumber==currentOpenCardNumber||randColor==currentOpenCardColor||randColor=="black"){
+             gameTurn="computer";
+             document.getElementById("playerCardView").style.pointerEvents = 'none';
+         }else{
+            gameTurn="player";
+            document.getElementById("playerCardView").style.pointerEvents = 'auto';
+         }
+     }
+ }else if(byWhom=="playerCardView"){
+     
+    if(playerCardCount>7){
+        debugger;
+        if(randamNumber==currentOpenCardNumber||randColor==currentOpenCardColor||randColor=="black"){
+            gameTurn="player";
+            document.getElementById("playerCardView").style.pointerEvents = 'auto';
+        }else{
+           gameTurn="computer";
+           document.getElementById("playerCardView").style.pointerEvents = 'nome';
+           computerMove();
+        }
+    }
+ }
 }
 
 function computerMove(){
     debugger;
+    if(gameTurn=="computer"){
     var count =document.getElementById("computerCardView").childElementCount;
     var loopcount=0;
-    if(playerTurn!=true)
     for(var i=1;i<=count;i++){
         console.log(document.getElementById("computerCardView").childElementCount);
         console.log(document.getElementById("computerCardView").childNodes[i].className);
         console.log(document.getElementById("computerCardView").childNodes[i].className.slice(9,11));
         computerCurrentCardNumber=document.getElementById("computerCardView").childNodes[i].className.slice(9,11);
-        computerCurrentCardColor=document.getElementById("computerCardView").childNodes[i].className.slice(11,17);
+        computerCurrentCardColor=document.getElementById("computerCardView").childNodes[i].className.slice(11,18);
         computerCurrentCardId=document.getElementsByClassName(document.getElementById("computerCardView").childNodes[i].className)[0].id;
-        console.log(document.getElementById("computerCardView").childNodes[i].className.slice(11,17));
+        console.log(document.getElementById("computerCardView").childNodes[i].className.slice(11,18));
         console.log(document.getElementsByClassName(document.getElementById("computerCardView").childNodes[i].className)[0].id);
-
-        if(computerCurrentCardNumber.trim()==currentOpenCardNumber.trim()||computerCurrentCardColor.trim()==currentOpenCardColor){
+        if(computerCurrentCardNumber.trim()==currentOpenCardNumber.trim()||computerCurrentCardColor.trim()==currentOpenCardColor||computerCurrentCardColor.trim()=="black"){
             console.log(computerCurrentCardNumber,computerCurrentCardColor,computerCurrentCardId);
-            
             removeCardFromView(computerCurrentCardId.trim(),computerCurrentCardNumber.trim(),computerCurrentCardColor.trim());
-            
             i=count+1;
             break;
         }
@@ -75,15 +99,7 @@ function computerMove(){
     if(loopcount==count){
         drawCard("computerCardView");
     }
-    if(currentOpenCardColor=="R"||currentOpenCardColor=="S"||currentOpenCardNumber=="+4"||currentOpenCardColor=="+2"){
-        playerTurn=true;
-        computerTurn==true;
-    }else{
-        computerTurn=false;
-        document.getElementById("playerCardView").style.pointerEvents = 'auto';
-
-    }
-    
+}
 }
 
 function execute(){
@@ -103,19 +119,29 @@ for(var i=0;i<7;i++){
 }
 function removeCardFromView(index,number,color){
     debugger;
+    if(currentOpenCardNumber=="p2"){
+        currentOpenCardNumber="+2"
+    }else if(currentOpenCardNumber=="p4"){
+        currentOpenCardNumber="+4";
+    }else if(number=="p2"){
+        number="+2"
+    }else if(number=="p4"){
+        number="+4";
+    }
     if(currentOpenCardColor==color||currentOpenCardNumber==number||color=="black"){
+        
         if(number=="+2"){
-            if(computerTurn==true){
+            if(gameTurn=="computer"){
                 drawCard("playerCardView");
                 drawCard("playerCardView");
             }
-            else{
+            else{ 
                 drawCard("computerCardView");
                 drawCard("computerCardView");
             }
             
         }else if(number=="+4"){
-            if(computerTurn==true){
+            if(gameTurn=="computer"){
                 drawCard("playerCardView");
                 drawCard("playerCardView");
                 drawCard("playerCardView");
@@ -129,35 +155,46 @@ function removeCardFromView(index,number,color){
             }
         }
         if(color=="black"){
+            wIndex=number;
+            if(gameTurn=="player"){
             document.getElementById('gameView').style.display='none';
             document.getElementById('entryForm').style.display='block';
             document.getElementById('entryForm').style.zIndex='9';
             document.getElementById('entryForm').innerHTML="<div class='entryForm' style='justify-content: center;align-items: center;padding-left: 80px;padding-top: 50px;'> Choose The Color:<br><select id='colorSelected'style='margin-left: 30px;margin-top: 30px;'><option value='red' style='background:red;'>Red</option><option value='green' style='background:green;'>Green</option><option value='yellow' style='background:yellow;'>Yellow</option><option value='blue' style='background:blue;'>Blue</option></select> <br><button style='margin-left: 32px;'onClick='changeDrawColor()'>Change</button></div>";
-        var remove=document.getElementById(index);
-        remove.parentNode.removeChild(remove);
-        droppedCardView(number,currentOpenCardColor);
-        }else{            
-        droppedCardView(number,color);
-        var remove=document.getElementById(index);
-        remove.parentNode.removeChild(remove);
-        }
+            }else{
+                currentOpenCardColor=colorsForCards[(Math.random() * colorsForCards.length-1) | 0];
+            }
+            var remove=document.getElementById(index);
+            remove.parentNode.removeChild(remove);
+            droppedCardView(number,currentOpenCardColor);
+            }else{            
+            droppedCardView(number,color);
+            var remove=document.getElementById(index);
+            remove.parentNode.removeChild(remove);
+            }
         
-        if(number=="S"||number=="R"||number=="+2"||number=="+4")
-            playerTurn=true;
-            else{
-                if(playerTurn==true){
-            playerTurn=false;
-            computerTurn=true;
-        }else{
-            playerTurn=true;
-            computerTurn=false;
-        }
-        }
-        console.log(playerTurn);
-    }
-    if(playerTurn==false){
-        document.getElementById("playerCardView").style.pointerEvents = 'none';
+        if(number=="S"||number=="R"||number=="+2"||number=="+4"){
+                if(gameTurn=="computer"){
+                    gameTurn="computer";
+                    document.getElementById("playerCardView").style.pointerEvents = 'none';
         computerMove();
+                }else{
+                    gameTurn="player";
+                    document.getElementById("playerCardView").style.pointerEvents = 'auto';
+                }
+            }else if(number!="W"){
+                if(gameTurn=="computer"){
+                    gameTurn="player";
+                    document.getElementById("playerCardView").style.pointerEvents = 'auto';
+        }else{
+            gameTurn="computer";
+            document.getElementById("playerCardView").style.pointerEvents = 'none';
+        computerMove();
+        }
+        }else if(number=="W"&&gameTurn=="computer"){
+                    document.getElementById("playerCardView").style.pointerEvents = 'auto';
+                    gameTurn="player";
+        }
     }
 }
 
@@ -167,21 +204,31 @@ function changeDrawColor(){
     droppedCardView(currentOpenCardNumber,changedColor);
     document.getElementById('entryForm').style.display='none';
     document.getElementById('gameView').style.display='block';
+    if(wIndex=="W"){
+        if(gameTurn=="computer"){
+            gameTurn="player";
+            document.getElementById("playerCardView").style.pointerEvents = 'auto';
+}else{
+    gameTurn="computer";
+    document.getElementById("playerCardView").style.pointerEvents = 'none';
+computerMove();
+}
+}
+wIndex="";
 }
 function droppedCardView(dropNumber,dropColor){
     var printnumber="";
+    if(dropNumber=="P2"){
+        dropNumber="+2";
+    }else if(dropNumber=="p4"){
+        dropNumber="+4";
+    }else{
+        printnumber=dropNumber; 
+    }
     if(dropNumber=="+2"){
         printnumber="p2";
     }else if(dropNumber=="+4"){
         printnumber="p4";
-    }else if(dropNumber=="p4"){
-        printnumber=dropNumber; 
-        dropNumber="+4";
-    }else if(dropNumber=="p2"){
-        printnumber=dropNumber; 
-        dropNumber="+2";
-    }else{
-        printnumber=dropNumber; 
     }
     currentOpenCardNumber=printnumber;
     currentOpenCardColor=dropColor ;
